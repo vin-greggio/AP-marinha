77acdab2ac0c04a3ed82d85ecf06c54d0ec2b1aa
miguellisboa58 <167940105+miguellisboa58@users.noreply.github.com>
2024-08-01 18:56:29 -0300

    Update pagina1.py
    tentativa do botão de colocar o pdf, ele ainda não faz nada com ele, até pq n sei como

--- pages/pagina1.py
+++ pages/pagina1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from PyPDF2 import PdfFileReader
 
 xls = pd.ExcelFile('big planilha.xlsx')
 planilha = st.selectbox('Selecione a planilha desejada', ['BP_Pagamento','Condomínio Papem', 'Taxa_de_Condomínio', 'Despesas', 'ReceitasxDespesas', 'Previsão orçamentaria', 'Taxa complementar'
@@ -15,6 +16,21 @@ planilha = st.selectbox('Selecione a planilha desejada', ['BP_Pagamento','Condom
                                                                             'Demonstrativo Fevereiro', 'Demonstrativo Março', 'Demonstrativo Abril', 'Demonstrativo Maio', 'Demonstrativo Junho'])
 df = pd.read_excel('big planilha.xlsx', sheet_name=planilha)
 df = st.data_editor(df, num_rows='dynamic')
+
+if planilha == 'Condomínio Papem':
+    pdf_ata = st.file_uploader('Coloque aqui o arquivo PDF (ata)')
+    if pdf_ata == None:
+        st.write('Arquivo ainda não selecionado')
+    else:
+        try:
+          with open(pdf_ata) as input_pdf:
+            pdf_reader = PdfFileReader(input_pdf)
+          if pdf_reader == False:
+              st.write('Arquivo ainda não selecionado')
+          else:
+            pdf_reader
+        except:
+           st.warning('Arquivo não compatível')
 
 
 
